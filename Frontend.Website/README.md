82ccc619484dfd4fcefbcef05990fc91d8045bb3
Tim Eitel <timeitel@outlook.com>
2021-06-13 09:50:15 +0800

    feat: add frontend site

---
+++ Frontend.Website/README.md
@@ -0,0 +1,11 @@
+## Full stack app testing vision APIs
+
+Tech stack:
+
+- Typescript
+- React
+- Node
+- Docker
+- CI / CD
+- Cloud Architecture
+- Microservices & Serverless
---
+++ Frontend.Website/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Vision App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>test</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+}
